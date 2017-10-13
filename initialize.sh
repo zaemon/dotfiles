99e9519e11431b16967fc0941fec39c52b9a70c9
vagrant <vagrant@vagrant.vm>
2017-10-13 11:05:14 +0900

    fcitxは諦めた。ibusで必要十分。 screen不要。byobuを使う。 xprofile不要。使わない内容。

--- initialize.sh
+++ initialize.sh
@@ -5,11 +5,8 @@ ln -sf ~/dotfiles/.docker ~/
 ln -sf ~/dotfiles/.gitconfig ~/
 ln -sf ~/dotfiles/.xprofile ~/
 ln -sf ~/dotfiles/.minttyrc ~/
-ln -sf ~/dotfiles/.screenrc ~/
 ln -sf ~/dotfiles/.vimrc ~/
 #ln -s ~/dotfiles/settings.json
 mkdir -p ~/.config/compiz-1/compizconfig
-mkdir -p ~/.config/fcitx
 ln -sf ~/dotfiles/.config/compiz-1/compizconfig/config ~/.config/compiz-1/compizconfig/
 ln -sf ~/dotfiles/.config/compiz-1/compizconfig/Default.ini ~/.config/compiz-1/compizconfig/
-ln -sf ~/dotfiles/.config/fcitx/config ~/.config/fcitx/
